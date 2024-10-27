ec795a1156993c2cd2280dd8e7f4797bd5b132b0
yaryan1303 <aryany1303@gmail.com>
2024-10-27 14:58:42 +0530

    first commit

---
+++ src/main/java/com/scm/scm_springproject/ScmScmSpringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.scm.scm_springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmScmSpringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmScmSpringprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scm/scm_springproject/ScmScmSpringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.scm_springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmScmSpringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
